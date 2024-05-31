4e36e4062bb5bde1ac82bfd2d349fef8c3c63d70
Per Jørgen Walstrøm <pjwalstrom+github@gmail.com>
2024-05-31 11:12:24 +0200

    Fix an issue where the score of the vulnerability is missing. Set the score to 0 instead

--- scripts/vulnerabilities.sh
+++ scripts/vulnerabilities.sh
@@ -22,10 +22,9 @@ for repo in $repos; do
     vulnerabilities=($(scorecard --checks=Vulnerabilities --show-details --format=json --repo=github.com/$1/$repo | jq 'select(.checks.[].details != null).checks.[].details.[]' | grep -oE -e "GHSA-[^ \"]*"))
     critical_count=0
     for vulnerability in $vulnerabilities; do
-        json=($(gh api --method GET -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /advisories -F ghsa_id="$vulnerability" --jq ".[].severity,.[].cvss.score,.[].html_url"))
+        json=($(gh api --method GET -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /advisories -F ghsa_id="$vulnerability" --jq ".[].severity,if .[].cvss.score != null then .[].cvss.score else 0 end,.[].html_url"))
         severity=$json[1]
-        if [[ $severity == "critical" ]] 
-        then
+        if [[ $severity == "critical" ]]; then
             critical_count=$((critical_count+1))            
             critical_count_tot=$((critical_count_tot+1))
             score=$json[2]
@@ -46,7 +45,7 @@ if [[ $critical_count_tot>0 ]]; then
     echo "Number of total critical vulnerabilities found in $1: $critical_count_tot" 
     for key val in "${(@kv)dictionary}"; do
         foo="occurrences"
-        if [[ $val == 1 ]];then
+        if [[ $val == 1 ]]; then
             foo="occurrence"
         fi
         echo "$key : $val $foo"
